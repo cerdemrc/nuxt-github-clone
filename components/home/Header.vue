db2d0742ab2c9c735e62ddeab41aaffe503f0e94
cerdemrc <cerdemrc@gmail.com>
2021-09-08 01:21:37 +0300

    fix input inside header

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -20,7 +20,24 @@
               ></path>
             </svg>
           </div>
-          <input type="text" placeholder="Search or jump to..." />
+          <div class="input-cage">
+            <input type="text" placeholder="Search or jump to..." />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="20"
+              aria-hidden="true"
+              class="mr-1 header-search-key-slash"
+            >
+              <path
+                fill="none"
+                stroke="#979A9C"
+                opacity=".4"
+                d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"
+              ></path>
+              <path fill="#979A9C" d="M11.8 6L8 15.1h-.9L10.8 6h1z"></path>
+            </svg>
+          </div>
           <div class="header-content-left-links">
             <ul>
               <li><a href="#">Pull requests</a></li>
@@ -180,11 +197,11 @@ export default {
       }
       & input {
         margin-right: 1rem;
-        background: var(--bg-header);
+        background: var(--bg-aside);
         border: none;
         border: 1px solid var(--border-color);
         border-radius: 6px;
-        width: 100%;
+        width: 260px;
         max-width: 100%;
         color: var(--text-primary);
         padding: 5px;
@@ -198,6 +215,14 @@ export default {
           padding: 5px;
           font-weight: 400;
           line-height: 2rem;
+        }
+      }
+      & .input-cage {
+        position: relative;
+        & svg {
+          position: absolute;
+          right: 12px;
+          top: 6px;
         }
       }
       &-links {
