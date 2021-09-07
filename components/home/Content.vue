a259389843999e88b1fae9fd51de29490be83162
cerdemrc <cerdemrc@gmail.com>
2021-09-07 18:43:11 +0300

    add footer cage

--- components/home/Content.vue
+++ components/home/Content.vue
@@ -4,17 +4,17 @@
       <h1>All activity</h1>
       <div class="content-left-repo">
         <div class="content-left-repo-title">
-          <img src="@/assets/profile.jpeg" />
+          <img src="@/assets/user2.jpeg" />
           <h2>
-            <a href="#">deepakgudi-pixel </a> starred
-            <a href="#"> kubowania/disney-clone-graphcms </a>
-            <span>7 hours ago</span>
+            <a href="#">stacy </a> starred
+            <a href="#"> projectmesa/mesa </a>
+            <span>1 hours ago</span>
           </h2>
         </div>
         <div class="content-left-repo-box">
           <div class="content-left-repo-box-title">
-            <a href="#">kubowania/disney-clone-graphcms</a>
-            <p>A demo to show how to use graphcms</p>
+            <a href="#">projectmesa/mesa</a>
+            <p>Mesa is an agent-based modeling framework in Js</p>
             <div class="content-left-repo-box-title-sub">
               <h1>
                 <div class="circle circle-yellow"></div>
@@ -66,15 +66,15 @@
         <div class="content-left-repo-title">
           <img src="@/assets/profile.jpeg" />
           <h2>
-            <a href="#">deepakgudi-pixel </a> starred
-            <a href="#"> kubowania/disney-clone-graphcms </a>
-            <span>7 hours ago</span>
+            <a href="#">cerdemrc </a> forked
+            <a href="#"> vue-cp-cheatsheet </a>
+            <span>3 hours ago</span>
           </h2>
         </div>
         <div class="content-left-repo-box">
           <div class="content-left-repo-box-title">
-            <a href="#">kubowania/disney-clone-graphcms</a>
-            <p>A demo to show how to use graphcms</p>
+            <a href="#">vue-cp-cheatsheet</a>
+            <p>interview prep cheatsheet and examples</p>
             <div class="content-left-repo-box-title-sub">
               <h1>
                 <div class="circle circle-green"></div>
@@ -95,7 +95,7 @@
                     d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
                   ></path>
                 </svg>
-                <span>3</span>
+                <span>12</span>
               </a>
               <p>Updated Sep 7</p>
             </div>
@@ -124,40 +124,19 @@
       <div class="separator"></div>
       <div class="content-left-repo">
         <div class="content-left-repo-title">
-          <img src="@/assets/profile.jpeg" />
+          <img src="@/assets/user.jpg" />
           <h2>
-            <a href="#">deepakgudi-pixel </a> starred
-            <a href="#"> kubowania/disney-clone-graphcms </a>
-            <span>7 hours ago</span>
+            <a href="#">george</a> created a repository
+            <a href="#">job-search-web-app</a>
+            <span>10 hours ago</span>
           </h2>
         </div>
         <div class="content-left-repo-box">
           <div class="content-left-repo-box-title">
-            <a href="#">kubowania/disney-clone-graphcms</a>
-            <p>A demo to show how to use graphcms</p>
+            <a href="#">job-search-web-app</a>
+            <p>Node.js | Cheerio | Express | React Js</p>
             <div class="content-left-repo-box-title-sub">
-              <h1>
-                <div class="circle circle-blue"></div>
-                Typescript
-              </h1>
-              <a href="#" class="stars">
-                <svg
-                  class="octicon octicon-star mr-1"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  height="16"
-                  fill="#8b949e"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                  ></path>
-                </svg>
-                <span>3</span>
-              </a>
-              <p>Updated Sep 7</p>
+              <p>Updated Aug 16</p>
             </div>
           </div>
           <div class="content-left-repo-box-button">
@@ -186,15 +165,15 @@
         <div class="content-left-repo-title">
           <img src="@/assets/profile.jpeg" />
           <h2>
-            <a href="#">deepakgudi-pixel </a> starred
-            <a href="#"> kubowania/disney-clone-graphcms </a>
+            <a href="#">cerdemrc</a> created a repository
+            <a href="#">open-library</a>
             <span>7 hours ago</span>
           </h2>
         </div>
         <div class="content-left-repo-box">
           <div class="content-left-repo-box-title">
-            <a href="#">kubowania/disney-clone-graphcms</a>
-            <p>A demo to show how to use graphcms</p>
+            <a href="#">open-library</a>
+            <p>the library application prepared using google book api</p>
             <div class="content-left-repo-box-title-sub">
               <h1>
                 <div class="circle circle-yellow"></div>
@@ -215,9 +194,9 @@
                     d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
                   ></path>
                 </svg>
-                <span>3</span>
+                <span>1</span>
               </a>
-              <p>Updated Sep 7</p>
+              <p>Updated Aug 2</p>
             </div>
           </div>
           <div class="content-left-repo-box-button">
@@ -242,62 +221,88 @@
         </div>
       </div>
       <div class="separator"></div>
-      <div class="content-left-repo">
-        <div class="content-left-repo-title">
-          <img src="@/assets/profile.jpeg" />
-          <h2>
-            <a href="#">deepakgudi-pixel </a> starred
-            <a href="#"> kubowania/disney-clone-graphcms </a>
-            <span>7 hours ago</span>
-          </h2>
-        </div>
-        <div class="content-left-repo-box">
-          <div class="content-left-repo-box-title">
-            <a href="#">kubowania/disney-clone-graphcms</a>
-            <p>A demo to show how to use graphcms</p>
-            <div class="content-left-repo-box-title-sub">
-              <h1>
-                <div class="circle circle-blue"></div>
-                Typescript
-              </h1>
-              <a href="#" class="stars">
-                <svg
-                  class="octicon octicon-star mr-1"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  height="16"
-                  fill="#8b949e"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                  ></path>
-                </svg>
-                <span>3</span>
-              </a>
-              <p>Updated Sep 7</p>
-            </div>
-          </div>
-          <div class="content-left-repo-box-button">
+      <div class="more-button">
+        <a href="#">More</a>
+      </div>
+      <div class="content-left-footer">
+        <p>
+          <svg
+            class="octicon octicon-light-bulb color-text-secondary"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+            fill="#8b949e"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 01-1.484.211c-.04-.282-.163-.547-.37-.847a8.695 8.695 0 00-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.75.75 0 01-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75zM6 15.25a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zM5.75 12a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-4.5z"
+            ></path>
+          </svg>
+          <span>
+            ProTip! The feed shows you events from people you
+            <a href="#">follow</a> and repositories you <a href="#">watch</a> or
+            <a href="#">star</a>.</span
+          >
+        </p>
+        <a href="#" class="subscribe"
+          ><svg
+            aria-hidden="true"
+            height="16"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            fill="#8b949e"
+            data-view-component="true"
+            class="octicon octicon-rss mr-1"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.002 2.725a.75.75 0 01.797-.699C8.79 2.42 13.58 7.21 13.974 13.201a.75.75 0 11-1.497.098 10.502 10.502 0 00-9.776-9.776.75.75 0 01-.7-.798zM2 13a1 1 0 112 0 1 1 0 01-2 0zm.84-5.95a.75.75 0 00-.179 1.489c2.509.3 4.5 2.291 4.8 4.8a.75.75 0 101.49-.178A7.003 7.003 0 002.838 7.05z"
+            ></path></svg
+          ><span> Subscribe to your news feed</span></a
+        >
+        <div class="content-left-footer-sub">
+          <div class="content-left-footer-sub-logo">
             <a href="#">
               <svg
-                class="octicon octicon-star mr-1"
+                aria-hidden="true"
+                height="24"
                 viewBox="0 0 16 16"
                 version="1.1"
-                width="16"
-                height="16"
-                fill="#8b949e"
-                aria-hidden="true"
+                width="24"
+                fill="#484f58"
+                data-view-component="true"
+                class="octicon octicon-mark-github d-block mr-2 float-left"
               >
                 <path
                   fill-rule="evenodd"
-                  d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
                 ></path>
               </svg>
-              <span>Star</span>
+              <span>@ 2021 GiHub, Inc.</span>
             </a>
+          </div>
+          <div class="content-left-footer-sub-links">
+            <ul class="links-box">
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Shop</a></li>
+              <li><a href="#">Contact GitHub</a></li>
+              <li><a href="#">Pricing</a></li>
+            </ul>
+            <ul class="links-box">
+              <li><a href="#">API</a></li>
+              <li><a href="#">Training</a></li>
+              <li><a href="#">Status</a></li>
+              <li><a href="#">Security</a></li>
+            </ul>
+            <ul class="links-box">
+              <li><a href="#">Terms</a></li>
+              <li><a href="#">Privacy</a></li>
+              <li><a href="#">Docs</a></li>
+            </ul>
           </div>
         </div>
       </div>
@@ -524,6 +529,99 @@ export default {};
             }
             &:hover {
               border-color: #8b949e;
+            }
+          }
+        }
+      }
+    }
+    & .more-button {
+      margin-top: 2rem;
+      width: 100%;
+      padding: 0.6rem;
+      background: var(--aside-color);
+      border-radius: 1rem;
+      border: 1px solid #30363d;
+      text-align: center;
+      & a {
+        color: var(--blue);
+        font-size: 1.4rem;
+        line-height: 2.1rem;
+        font-weight: 600;
+      }
+      &:hover {
+        background: #161b22;
+      }
+    }
+    &-footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      margin-top: 2rem;
+      p,
+      .subscribe {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.2rem;
+        font-weight: 400;
+        line-height: 1.8rem;
+        color: #8b949e;
+        & span {
+          margin-left: 3px;
+        }
+        & a {
+          color: var(--blue);
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+      .subscribe {
+        margin-top: 1rem;
+        &:hover {
+          color: var(--blue);
+          text-decoration: underline;
+        }
+      }
+      &-sub {
+        display: flex;
+        margin-top: 4rem;
+        &-logo a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          & svg:hover {
+            fill: #c9d1d9;
+          }
+          & span {
+            margin-left: 1rem;
+            font-size: 1.2rem;
+            line-height: 1.8rem;
+            font-weight: 400;
+            color: #8b949e;
+          }
+        }
+        &-links {
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          margin-left: 15rem;
+          & .links-box {
+            list-style-type: none;
+            padding-right: 2.5rem;
+            & li {
+              padding: 2px 0;
+              & a {
+                font-size: 1.2rem;
+                line-height: 1.8rem;
+                font-weight: 400;
+                color: #8b949e;
+                &:hover {
+                  color: var(--blue);
+                  text-decoration: underline;
+                }
+              }
             }
           }
         }
