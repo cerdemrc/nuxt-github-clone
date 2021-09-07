74226d0c9b034b7c3307ae1719d50dae1c49dafe
cerdemrc <cerdemrc@gmail.com>
2021-09-07 15:01:41 +0300

    create aside component

---
+++ components/home/Aside.vue
@@ -0,0 +1,286 @@
+<template>
+  <aside>
+    <div class="aside">
+      <div class="aside-content">
+        <div class="aside-content-title">
+          <h1>Repositories</h1>
+          <a href="#" class="button--green">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#F0F6FC"
+              data-view-component="true"
+              class="octicon octicon-repo"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+              ></path>
+            </svg>
+            New</a
+          >
+        </div>
+        <input type="text" placeholder="Find a repository..." />
+        <div class="aside-content-repos">
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#d29922"
+              data-view-component="true"
+              class="octicon octicon-lock"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4v2h-.25A1.75 1.75 0 002 7.75v5.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-5.5A1.75 1.75 0 0012.25 6H12V4a4 4 0 10-8 0zm6.5 2V4a2.5 2.5 0 00-5 0v2h5zM12 7.5h.25a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-5.5a.25.25 0 01.25-.25H12z"
+              ></path>
+            </svg>
+            cerdemrc/portfolio-vue
+          </a>
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#d29922"
+              data-view-component="true"
+              class="octicon octicon-lock"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4v2h-.25A1.75 1.75 0 002 7.75v5.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-5.5A1.75 1.75 0 0012.25 6H12V4a4 4 0 10-8 0zm6.5 2V4a2.5 2.5 0 00-5 0v2h5zM12 7.5h.25a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-5.5a.25.25 0 01.25-.25H12z"
+              ></path>
+            </svg>
+            cerdemrc/nuxt-express-starter
+          </a>
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#A0A6AC"
+              data-view-component="true"
+              class="octicon octicon-repo"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+              ></path>
+            </svg>
+            cerdemrc/taskello
+          </a>
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#d29922"
+              data-view-component="true"
+              class="octicon octicon-lock"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4v2h-.25A1.75 1.75 0 002 7.75v5.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-5.5A1.75 1.75 0 0012.25 6H12V4a4 4 0 10-8 0zm6.5 2V4a2.5 2.5 0 00-5 0v2h5zM12 7.5h.25a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-5.5a.25.25 0 01.25-.25H12z"
+              ></path>
+            </svg>
+            cerdemrc/portfolio-vue
+          </a>
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#d29922"
+              data-view-component="true"
+              class="octicon octicon-lock"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4v2h-.25A1.75 1.75 0 002 7.75v5.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-5.5A1.75 1.75 0 0012.25 6H12V4a4 4 0 10-8 0zm6.5 2V4a2.5 2.5 0 00-5 0v2h5zM12 7.5h.25a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-5.5a.25.25 0 01.25-.25H12z"
+              ></path>
+            </svg>
+            cerdemrc/nuxt-express-starter
+          </a>
+          <a href="#">
+            <svg
+              aria-label="Repository"
+              role="img"
+              height="16"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              fill="#A0A6AC"
+              data-view-component="true"
+              class="octicon octicon-repo"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+              ></path>
+            </svg>
+            cerdemrc/taskello
+          </a>
+        </div>
+        <div class="show-more">
+          <a href="#">Show more</a>
+        </div>
+        <div class="white-line"></div>
+        <div class="sub-info">
+          <h1>Recent activity</h1>
+          <p>
+            When you take actions across GitHub, we’ll provide links to that
+            activity here.
+          </p>
+        </div>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.aside {
+  background: var(--aside-color);
+  max-width: 35rem;
+  position: fixed;
+  left: 0;
+  height: 100vh;
+  overflow-y: hidden;
+  &-content {
+    padding: 30px;
+    &-title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      & h1 {
+        font-weight: 600;
+        font-size: 1.4rem;
+        color: var(--repo-color);
+        line-height: 2.1rem;
+      }
+      & .button--green {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: var(--green);
+        color: var(--white);
+        font-weight: 600;
+        font-size: 1.2rem;
+        line-height: 2rem;
+        padding: 4px 10px;
+        border-radius: 6px;
+        text-decoration: none;
+        & svg {
+          margin-right: 3px;
+        }
+        &:hover {
+          background: var(--hover-green);
+        }
+      }
+    }
+    & input {
+      margin-top: 1rem;
+      background: var(--main-color);
+      border: none;
+      border: 1px solid var(--input-border);
+      border-radius: 6px;
+      width: 100%;
+      max-width: 100%;
+      color: var(--repo-color);
+      padding: 5px;
+      font-weight: 400;
+      line-height: 2rem;
+      &:focus {
+        outline: none;
+      }
+      &::placeholder {
+        color: var(--repo-color);
+        opacity: 0.4;
+        padding: 5px;
+        line-height: 2rem;
+      }
+    }
+    &-repos {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      margin-top: 1.5rem;
+      & a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: var(--blue);
+        text-decoration: none;
+        font-size: 1.4rem;
+        line-height: 2.1rem;
+        font-weight: 600;
+        margin-top: 8px;
+        &:hover {
+          text-decoration: underline;
+        }
+        & svg {
+          margin-right: 6px;
+        }
+      }
+    }
+    & .show-more {
+      margin-top: 2rem;
+      & a {
+        color: #8b949e;
+        font-size: 1.2rem;
+        font-weight: 400;
+        line-height: 1.8rem;
+        text-decoration: none;
+        &:hover {
+          color: var(--blue);
+        }
+      }
+    }
+    & .white-line {
+      margin: 2rem 0;
+      background: var(--white);
+      opacity: 0.3;
+      width: 100%;
+      height: 1px;
+    }
+    & .sub-info {
+      margin-top: 2rem;
+      & h1 {
+        font-weight: 600;
+        color: var(--repo-color);
+        line-height: 2.1rem;
+      }
+      & p {
+        color: #8b949e;
+        margin-top: 1rem;
+        font-weight: 400;
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+      }
+    }
+  }
+}
+</style>
